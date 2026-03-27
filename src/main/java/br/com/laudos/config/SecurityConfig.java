3c611a24147a2524ef338ba82f43ca46395be902
Emygdio Cavalcanti Toscano Barreto <emygdio.barreto@gmail.com>
2026-03-27 11:21:53 -0300

    Ajuste em endpoind para liberação de acesso externo

--- src/main/java/br/com/laudos/config/SecurityConfig.java
+++ src/main/java/br/com/laudos/config/SecurityConfig.java
@@ -51,9 +51,9 @@ public class SecurityConfig  {
                         .requestMatchers("/auth/**").permitAll() // público, acessado sem autenticação
                         //.requestMatchers("/**/auth/register").permitAll() // público, acessado sem autenticação
                         .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers("/laudos/validar/**").permitAll()
+                        //.requestMatchers("/laudos/validar/**").permitAll()
                         .requestMatchers("/api/laudos/validar/**").permitAll()
-                        .requestMatchers("/validar/**").permitAll()
+                        //.requestMatchers("/validar/**").permitAll()
                         .requestMatchers("/laudos/validar-dados/**").permitAll()
                         .requestMatchers(WHITE_LIST_URL).permitAll()
                         .anyRequest().authenticated()
